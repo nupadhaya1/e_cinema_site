71c375093b0f7b3ac6307ddf429799245fb55bfa
Jeffrey <jeffaaaaaah@gmail.com>
2025-02-24 19:12:23 -0500

    movietest

---
+++ src/app/movietest/page.tsx
@@ -0,0 +1,11 @@
+import SelectMovieButton from "~/components/booking/SelectMovieButton";
+import { Movie } from "~/components/booking/SelectMovieButton";
+
+const m: Movie = {
+  id: 1,
+  title: "sonic",
+  image: "",
+};
+export default function P() {
+  return <SelectMovieButton selectedMovie={m}></SelectMovieButton>;
+}
