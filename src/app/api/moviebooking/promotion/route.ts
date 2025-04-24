222507acfaf0a60dc33db33f0d044a9aa06f62b9
Nikhil Upadhaya <nikhil.upadhaya001@gmail.com>
2025-04-24 16:36:21 -0400

    add promotions fix

--- src/app/api/moviebooking/promotion/route.ts
+++ src/app/api/moviebooking/promotion/route.ts
@@ -16,20 +16,60 @@ export async function GET(request: Request) {
 
   try {
     let data = await db.query.promotions.findFirst({
-        where: (promotions, {eq}) => eq(promotions.code, code+"")
-    })
+      where: (promotions, { eq }) => eq(promotions.code, code + ""),
+    });
 
     if (!data) {
-      return NextResponse.json({ error: "promotion not found" }, { status: 404 });
+      return NextResponse.json(
+        { error: "promotion not found" },
+        { status: 404 },
+      );
     }
 
-    return NextResponse.json(
-      [data.discount]
-    );
+    return NextResponse.json([data.discount]);
   } catch (error) {
     console.error("Error fetching user:", error);
     return NextResponse.json(
       { error: "Failed to fetch user" },
+      { status: 500 },
+    );
+  }
+}
+export async function POST(req: Request) {
+  const user = await auth();
+  if (!user || !user.userId) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const body = await req.json();
+
+  if (body.delete && body.code) {
+    try {
+      await db.delete(promotions).where(eq(promotions.code, body.code));
+      return NextResponse.json({ message: "Promotion deleted" });
+    } catch (err) {
+      return NextResponse.json(
+        { error: "Error deleting promotion" },
+        { status: 500 },
+      );
+    }
+  }
+
+  if (!body.code || typeof body.discount !== "number") {
+    return NextResponse.json({ error: "Invalid input" }, { status: 400 });
+  }
+
+  try {
+    await db.insert(promotions).values({
+      code: body.code,
+      discount: body.discount,
+    });
+
+    return NextResponse.json({ message: "Promotion added" });
+  } catch (err) {
+    console.error("Insert error:", err);
+    return NextResponse.json(
+      { error: "Failed to add promotion" },
       { status: 500 },
     );
   }
